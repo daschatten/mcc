e89cad89d68b2be1c43ac75ba2233cf202f0d300
Florian Bittner <dev@daschatten.de>
2014-01-04 14:28:39 +0100

    Integrated extension yiistrap.

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -16,6 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <?php Yii::app()->bootstrap->register(); ?>
 </head>
 
 <body>
