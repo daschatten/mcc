311051f73eb4a3eed6b98a0f8c14c56b57e7f647
Florian Bittner <dev@daschatten.de>
2014-01-05 02:46:03 +0100

    Added basic auth.php to extras directory.

---
+++ extra/auth.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+  'admin' => 
+  array (
+    'type' => 2,
+    'description' => 'Administrator role',
+    'bizRule' => NULL,
+    'data' => NULL,
+    'assignments' => 
+    array (
+      1 => 
+      array (
+        'bizRule' => NULL,
+        'data' => NULL,
+      ),
+    ),
+  ),
+);
