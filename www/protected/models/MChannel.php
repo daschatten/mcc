0304f499b70d750e58c88fea93b10eb7375bfdb9
Florian Bittner <dev@daschatten.de>
2014-01-05 23:22:52 +0100

    Added models for 'recorded' and 'channel'.

---
+++ www/protected/models/MChannel.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('application.models._base.BaseMChannel');
+
+class MChannel extends BaseMChannel
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
