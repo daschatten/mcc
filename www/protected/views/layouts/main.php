ae3bcee8d9fb300376b7d4ff67b07490bb8dc4b4
Florian Bittner <dev@daschatten.de>
2014-01-06 20:19:28 +0100

    Modified footer for copyright information.

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -64,7 +64,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Florian Bittner<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
