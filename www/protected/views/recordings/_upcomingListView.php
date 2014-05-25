751917e247f85f3a3a7d37858bc3b0577296f57f
Florian Bittner <dev@daschatten.de>
2014-05-25 22:00:26 +0200

    Fix translation and layout.

--- www/protected/views/recordings/_upcomingListView.php
+++ www/protected/views/recordings/_upcomingListView.php
@@ -3,15 +3,37 @@
 $this->widget('bootstrap.widgets.TbGridView', array(
     'dataProvider'=>$dataProvider,
     'columns' => array(
-        'Recording.EncoderId',
+        array(
+            'name' => 'Recording.EncoderId',
+            'header' => Yii::t('app', 'Tuner'),
+            'htmlOptions' => array('class' => 'tdcenter'),
+            'headerHtmlOptions' => array('class' => 'tdcenter'),
+        ),
         array(
             'name' => 'StartTime',
+            'header' => Yii::t('app', 'Time'),
             'value' => 'Yii::app()->dateFormatter->formatDateTime(strtotime($data->StartTime), "short", "short")',
+            'htmlOptions' => array('class' => 'tdcenter'),
+            'headerHtmlOptions' => array('class' => 'tdcenter'),
         ),
-        'Channel.ChannelName',
-        'Title',
-        'SubTitle',
-        'Recording.RecGroup',
+        array(
+            'name' => 'Channel.ChannelName',
+            'header' => Yii::t('app', 'Channel'),
+        ),
+        array(
+            'name' => 'Title',
+            'header' => Yii::t('app', 'Title'),
+        ),
+        array(
+            'name' => 'SubTitle',
+            'header' => Yii::t('app', 'Subtitle'),
+        ),
+        array(
+            'name' => 'Recording.RecGroup',
+            'header' => Yii::t('app', 'Group'),
+            'htmlOptions' => array('class' => 'tdcenter'),
+            'headerHtmlOptions' => array('class' => 'tdcenter'),
+        ),
     ),
 ));
 
