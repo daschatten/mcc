b46869adf3f6974f6538861e48c2586517f8ff91
Florian Bittner <dev@daschatten.de>
2014-07-31 20:44:00 +0200

    Add channel relation.

--- www/protected/models/mythtv/Oldrecorded.php
+++ www/protected/models/mythtv/Oldrecorded.php
@@ -7,4 +7,12 @@ class Oldrecorded extends BaseOldrecorded
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
+    
+    public function relations()
+    {
+        return array(
+            'channel'=>array(self::BELONGS_TO, 'Channel', 'chanid'),
+        );
+    }
+
 }
